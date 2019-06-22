af99761c7470abbbbd1840a603dd226ad5c4f0a2
Nikita Karatsev <nikita.karatsev@gmail.com>
2019-06-22 15:14:47 +0600

    add in draw()

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 #os.getcwd()
 #import vk_api
-#from interface.size import Size
 from src.size import ConsoleSize
 import curses
 from curses import wrapper
@@ -16,6 +15,8 @@ def main(stdscr):
 
     textbox1.draw(size.getSize())
     stdscr.getkey()
+
+wrapper(main)
     #stdscr = curses.initscr()
     #stdscr.clear()
     # stdscr.addstr(0, 0, "Current mode: Typing mode",
@@ -26,18 +27,7 @@ def main(stdscr):
     # stdscr.getkey()
     # stdscr = curses.initscr()
     # stdscr.clear()
-    #
-    # for i in range(0, 10):
-    #     v = i - 10
-    #     stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10 / v))
-    #
-    # stdscr.refresh()
-    # stdscr.getkey()
 
-wrapper(main)
-    # size = ConsoleSize()
-    # if(size.isSizeWork()):
-    #     print(size.getSize())
     # vk_session = vk_api.VkApi(login, password, token = token, scope = '4096')
     # try:
     #     vk_session.auth(token_only=True)
@@ -51,21 +41,6 @@ wrapper(main)
     #wall = tools.get_all('wall.get', 100, {'owner_id': 1})
     #friends = vk.friends.get(count = 10, order= 'name', fields = 'id,first_name,last_name')
     #response = vk.wall.get(count=1)
-    #size = Size()
-    #size.getSize()
-    #size.printSize()
-    # if response['items']:
-    #     print(response['items'][0])
-    # print("===============================================")
-
-
-    #size = Size()
-    #_rows , _columns = size.getSize()
-    #size = Size()
-    #print(size.getSize())
-    #lol = os.popen('stty size', 'r').read().split()
-    #print(lol)
-    #print("rows: ", {}, "columns:", {}).format(_rows, _columns)
 
 
 if __name__ == '__main__':
--- src/tui/boxes/textbox.py
+++ src/tui/boxes/textbox.py
@@ -2,12 +2,14 @@ from src.tui.control import Control
 from src.size import ConsoleSize
 import curses
 import curses.panel
-#I don't know why I'll add curses.panel
+
+
+# I don't know why I'll add curses.panel
 
 
 class TextBox(Control):
 
-    def __init__(self, width, height, position, type = 'ascii'):
+    def __init__(self, width, height, position, type='ascii'):
         self.stdscr = curses.initscr()
         self.stdscr.keypad(True)
         self.height = height
@@ -17,10 +19,12 @@ class TextBox(Control):
 
     def draw(self, size):
         for i in range(0, self.width):
-            self.stdscr.addstr(self.position[0], self.position[1] + i, "*")
-            self.stdscr.addstr(self.position[0] + self.height, self.position[1] + i, "*")
-
-        #for
+            self.stdscr.addstr(self.position[0], self.position[1] + i, "-")
+            self.stdscr.addstr(self.position[0] + self.height, self.position[1] + i, "-")
+        for i in range(1, self.height):
+            self.stdscr.addstr(self.position[0] + i, self.position[1], "|")
+            self.stdscr.addstr(self.position[0] + i, self.position[1] + self.width - 1, "|")
+        # for
 
     def update(self):
         pass
